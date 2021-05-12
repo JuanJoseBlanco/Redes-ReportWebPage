310fe0bb2a0b86f0f9b58b745f8e7ab0d12c63e6
Juan José Blanco <juan.blanco03@uptc.edu.co>
2021-05-12 09:32:11 -0500

    Finished Home Page and Added other pages

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/", express.static(__dirname + "/public"));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
