e7c3a1edf8efb4c29563d928308f6af1afd0aa02
Juan José Blanco <juan.blanco03@uptc.edu.co>
2021-05-17 19:32:29 -0500

    Some funct

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use("/", express.static(__dirname + "/public"));
 
+app.get('/', (req, res) => {
+  res.send('Servidor test corriendo!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
